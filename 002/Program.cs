33abb177eddcdafe7611ab0e0a7217041fe15e08
Andrey <andslob@inbox.ru>
2023-02-01 09:48:44 +0500

    002

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine(array[3]);
+// [4, 5, 3, 1, 2]
+// O(n)
+// [1, 2, 3, 4, 5] - O(n * log n)
+// ((5 + 1)/2) * 5 = O(1)
+// n < n * log(n) + 1
+int summa = 0;
+for (int i = 0; i < n; i++)
+    suuma += array[i];
+Console.WriteLine(summa);
+// O(n^2)
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= n; i++)
+{
+    for(int j = 1; j <= n; j++)
+    {
+        Console.Write(i * j);
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+}
+// O(n^2 / 2)
+int n = Convert.ToInt32(Console.ReadLine());
+int [, ] matrix = new int[n, m];
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);    
+    }
+    Console.WriteLine();
+}
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j]);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
